4241fd6bf3843b45ed2fd902cc0abdb93f0b4611
Jinu Joy <jinuparekatil@gmail.com>
2023-12-04 15:09:32 +0530

    new app, navigation bar item

---
+++ GoodWeather/Controllers/AddweatherCityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddweatherCityViewController.swift
+//  GoodWeather
+//
+//  Created by Jinu on 04/12/2023.
+//
+
+import Foundation
+import UIKit
+
+class AddweatherCityViewController: UIViewController{
+    
+}
---
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by Jinu on 04/12/2023.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController: UITableViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+}
